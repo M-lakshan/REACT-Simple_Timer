ca618f56226fed9658601a87b6c6a0a4babf4979
M-lakshan <emlakshan92@gmail.com>
2024-05-20 21:46:20 +0530

    change styling logics in #time-left : added animations + useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,13 @@ import '../styles/App.css';
 
 function App() {
 
-  const [SessionTime,setSessionTime] = useState(1500);
-  const [SessionBreak,setSessionBreak] = useState(300);
-  const [SessionLength,setSessionLength] = useState(1500);
-  const [SessionState,setSessionState] = useState("stopped");
-  const [SessionType,setSessionType] = useState("countdown");
-  const [SessionOnPause,setSessionOnPause] = useState(false);
+  const [sessionTime,setSessionTime] = useState(1500);
+  const [sessionBreak,setSessionBreak] = useState(300);
+  const [sessionLength,setSessionLength] = useState(1500);
+  const [sessionState,setSessionState] = useState("stopped");
+  const [sessionType,setSessionType] = useState("countdown");
+  const [sessionOnPause,setSessionOnPause] = useState(false);
+  const [timeLeftStyle,setTimeLeftStyle] = useState({ color: "#FFF", backgroundColor: "#2e8cd4" });
 
   const UpdateDurations = (value,type) => {
 
@@ -25,19 +26,19 @@ function App() {
     
     const timer = setInterval(() => {
 
-      if(SessionState==="running" && !SessionOnPause) {
+      if(sessionState==="running" && !sessionOnPause) {
 
-        if(SessionTime-1===0) {
+        if(sessionTime-1===0) {
           document.getElementById("beep").play();
         }
 
-        if(SessionTime===0) {
+        if(sessionTime===0) {
           clearInterval(timer);
 
           setTimeout(() => ManageSession("change"),1000);
         } else {
           setTimeout(() => document.getElementById("time-left").classList.remove("activated"), 1000);
-          setSessionTime(SessionTime-1);
+          setSessionTime(sessionTime-1);
         }
       }
     },1000);
@@ -46,30 +47,7 @@ function App() {
   });
 
   const ManageSession = (type) => {
-/*
-    let countdown = document.getElementById("time-left");
-    
-    if(SessionState==="stopped") {
-      countdown.classList.add("activated");
-    } else {
 
-      if(SessionType!=="break") {
-
-        if(!SessionOnPause) {
-          countdown.classList.add("postponed");
-        } else {
-          (type==="reset") ? countdown.classList.add("stopped") : countdown.classList.add("activated");
-        }
-      }
-    }
-
-    setTimeout(() => {
-      countdown.classList.remove("activated");
-      countdown.classList.remove("postponed");
-      countdown.classList.remove("stopped");
-      (type==="reset") && countdown.classList.remove("active");
-    }, 1000);
-*/
     if(type==="reset") {
       document.getElementById("beep").pause();
       document.getElementById("beep").currentTime = 0;
@@ -80,42 +58,48 @@ function App() {
       setSessionBreak(300);
       setSessionLength(1500);
       setSessionTime(1500);
+      setTimeLeftStyle({ animation: "on_stop 2s linear 0.2s 1 normal forwards" });
     } else if(type==="pause") {
-      (SessionType!=="break") && setSessionOnPause(!SessionOnPause);
+      (sessionType!=="break") && setTimeLeftStyle({ animation: "on_pause 0.35s linear 0s 1 normal forwards" });
+      (sessionType!=="break") && setSessionOnPause(!sessionOnPause);
     } else if(type==="change") {
-      setSessionType((SessionType==="countdown") ? "break" : "countdown");  
-      setSessionTime(((SessionType==="countdown") ? SessionBreak : SessionLength));
+      let sessionOnBreak = (sessionType==="break");
+      let customAnimation = (sessionOnBreak) ? "on_start 2s ease-in-out" : "time_jump 1.2s linear";
+
+      setTimeLeftStyle({ animation: `${customAnimation} 0s 1 normal forwards` });
+      setSessionType((sessionOnBreak) ? "countdown" : "break");  
+      setSessionTime((sessionOnBreak) ? sessionLength : sessionBreak);
     } else {
+      setTimeLeftStyle({ animation: "on_start 2.5s ease-in-out 0s 1 normal forwards" });
       setSessionOnPause(false);
       setSessionState("running");
     }
   }
 
-  let ses_running = (SessionState==="running");
-  let ses_on_countdown = (SessionType==="countdown");
-  let param = ((!ses_running) ? "start" : (SessionOnPause ? "start" : "pause"));
-  let btn_cls = (!ses_running ? "play" : ((SessionOnPause) ? "play" : "pause"));
-  let minutes = Math.floor(SessionTime / 60);
-  let seconds = SessionTime % 60;
+  let ses_running = (sessionState==="running");
+  let ses_on_countdown = (sessionType==="countdown");
+  let param = ((!ses_running) ? "start" : (sessionOnPause ? "start" : "pause"));
+  let btn_cls = (!ses_running ? "play" : ((sessionOnPause) ? "play" : "pause"));
+  let minutes = Math.floor(sessionTime / 60);
+  let seconds = sessionTime % 60;
   
   return (
     <div id="Timer" className="App">
       <Timeset 
         setTimeFrame={(e) => UpdateDurations(e,"break")}
         _type="break"
-        _val={SessionBreak}
+        _val={sessionBreak}
       />
       <Timeset 
         setTimeFrame={(e) => UpdateDurations(e,"session")}
         _type="session"
-        _val={SessionLength}
+        _val={sessionLength}
       />
       <section className="timer">
         <div className="container">
           <h3 id="timer-label">{(ses_on_countdown) ? "Session" : "Break"}</h3>
-          <p id="time-left" 
-            className={(ses_running) ? ((ses_on_countdown) ? "active activated" : "on_break") : "deactivated"}>
-              {minutes < 10 ? '0'+minutes : minutes}:{seconds < 10 ? '0'+seconds : seconds}
+          <p id="time-left" style={timeLeftStyle}>
+            {minutes < 10 ? '0'+minutes : minutes}:{seconds < 10 ? '0'+seconds : seconds}
           </p>
         </div>
         <div className="controls">

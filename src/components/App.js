4274874094d892779776026b25598928af61a454
M-lakshan <emlakshan92@gmail.com>
2024-05-21 13:19:02 +0530

    useRef, useEffect updated but not working properly

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Timeset from './Timeset';
 import '../styles/App.css';
 
 function App() {
 
-  const [sessionTime,setSessionTime] = useState(1500);
-  const [sessionBreak,setSessionBreak] = useState(300);
+  const audRef = useRef(null);
+  const [sessionTimer,setSessionTimer] = useState(null);
+  const [sessionTime,setSessionTime] = useState(1);
+  const [sessionBreak,setSessionBreak] = useState(15);
   const [sessionLength,setSessionLength] = useState(1500);
   const [sessionState,setSessionState] = useState("stopped");
   const [sessionType,setSessionType] = useState("countdown");
-  const [sessionOnPause,setSessionOnPause] = useState(false);
+  const [sessionPaused,setSessionPause] = useState(false);
   const [timeLeftStyle,setTimeLeftStyle] = useState({ color: "#FFF", backgroundColor: "#2e8cd4" });
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    
+    if(audRef.current) {
+      if(isPlaying) {
+        audRef.current.pause();
+      } else {
+        audRef.current.play();
+      }
+
+      setIsPlaying(!isPlaying);
+    }
+
+    return;
+  };
 
   const UpdateDurations = (value,type) => {
 
@@ -21,30 +39,30 @@ function App() {
       setSessionLength(value*60);
     } 
   }
-
+ 
   useEffect(() => {
     
-    const timer = setInterval(() => {
+    if(sessionState==="running" && !sessionPaused) {
+      
+      const timerID = setInterval(() => {
 
-      if(sessionState==="running" && !sessionOnPause) {
-
-        if(sessionTime-1===0) {
-          document.getElementById("beep").play();
-        }
-
-        if(sessionTime===0) {
-          clearInterval(timer);
-
-          setTimeout(() => ManageSession("change"),1000);
-        } else {
-          setTimeout(() => document.getElementById("time-left").classList.remove("activated"), 1000);
-          setSessionTime(sessionTime-1);
-        }
-      }
-    },1000);
-
-    return () => clearInterval(timer);
-  });
+        setSessionTime((prevTime) => {
+          if(prevTime > 0) {
+            return prevTime - 1;
+          } else {
+            handlePlayPause();
+            ManageSession("change");
+            return 0;
+          }
+        });
+      },1000);
+      
+      setSessionTimer(timerID);
+      
+      return () => clearInterval(timerID);
+    }
+  },[sessionState,sessionTime,sessionPaused]);
+  // });
 
   const ManageSession = (type) => {
 
@@ -52,7 +70,8 @@ function App() {
       document.getElementById("beep").pause();
       document.getElementById("beep").currentTime = 0;
       
-      setSessionOnPause(false);
+      setSessionTimer(null);
+      setSessionPause(false);
       setSessionState("stopped");
       setSessionType("countdown");
       setSessionBreak(300);
@@ -60,26 +79,30 @@ function App() {
       setSessionTime(1500);
       setTimeLeftStyle({ animation: "on_stop 2s linear 0.2s 1 normal forwards" });
     } else if(type==="pause") {
+      setSessionTimer(null);
       (sessionType!=="break") && setTimeLeftStyle({ animation: "on_pause 0.35s linear 0s 1 normal forwards" });
-      (sessionType!=="break") && setSessionOnPause(!sessionOnPause);
+      (sessionType!=="break") && setSessionPause(!sessionPaused);
     } else if(type==="change") {
       let sessionOnBreak = (sessionType==="break");
       let customAnimation = (sessionOnBreak) ? "on_start 2s ease-in-out" : "time_jump 1.2s linear";
 
+      clearInterval(sessionTimer);
       setTimeLeftStyle({ animation: `${customAnimation} 0s 1 normal forwards` });
       setSessionType((sessionOnBreak) ? "countdown" : "break");  
       setSessionTime((sessionOnBreak) ? sessionLength : sessionBreak);
+
+      return;
     } else {
       setTimeLeftStyle({ animation: "on_start 2.5s ease-in-out 0s 1 normal forwards" });
-      setSessionOnPause(false);
+      setSessionPause(false);
       setSessionState("running");
     }
   }
 
   let ses_running = (sessionState==="running");
   let ses_on_countdown = (sessionType==="countdown");
-  let param = ((!ses_running) ? "start" : (sessionOnPause ? "start" : "pause"));
-  let btn_cls = (!ses_running ? "play" : ((sessionOnPause) ? "play" : "pause"));
+  let param = ((!ses_running) ? "start" : (sessionPaused ? "start" : "pause"));
+  let btn_cls = (!ses_running ? "play" : ((sessionPaused) ? "play" : "pause"));
   let minutes = Math.floor(sessionTime / 60);
   let seconds = sessionTime % 60;
   
@@ -109,7 +132,12 @@ function App() {
           <button id="reset" onClick={() => ManageSession("reset")}>
             <i className="fa-solid fa-rotate"></i>
           </button>
-          <audio id="beep" preload="auto" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/BeepSound.wav"></audio>
+          <audio 
+            id="beep"
+            preload="auto"
+            ref={audRef}
+            src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/BeepSound.wav"
+          ></audio>
         </div>
       </section>
     </div>
